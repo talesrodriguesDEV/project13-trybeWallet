f054c26e4d7ad54c0826982ed8e05fc22d0f03da
Tales Rodrigues <tales00001@gmail.com>
2022-08-02 16:48:32 -0300

    Updates

---
+++ precache-manifest.292db333460efeda360bea5ebff29e84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a74f843c360789077b58687c79f2f92",
+    "url": "/project13-trybeWallet/index.html"
+  },
+  {
+    "revision": "fe7d2718f1116b8799bb",
+    "url": "/project13-trybeWallet/static/css/main.06c3b7d4.chunk.css"
+  },
+  {
+    "revision": "b6e177c7a499ebd29ab3",
+    "url": "/project13-trybeWallet/static/js/2.29b32461.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project13-trybeWallet/static/js/2.29b32461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe7d2718f1116b8799bb",
+    "url": "/project13-trybeWallet/static/js/main.c8bbc9c5.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/project13-trybeWallet/static/js/main.c8bbc9c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06a09a93d82495fb1773",
+    "url": "/project13-trybeWallet/static/js/runtime-main.b3cc85a4.js"
+  },
+  {
+    "revision": "3de1c272901b37adcba6dd8a68fc368e",
+    "url": "/project13-trybeWallet/static/media/pexels-john-guccione-wwwadvergroupcom-3483098.3de1c272.jpg"
+  }
+]);
